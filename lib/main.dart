ada70fb0655e1c7a2b33f100056bfae0fa89a089
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-10-30 22:38:15 -0500

    Flutter code into separate files and folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:counter_application/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(
-        child: Text('Hi World'),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
